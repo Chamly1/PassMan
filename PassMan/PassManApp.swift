9b445f471e9175c12970f0f4ca94fffddeb2013e
Vladyslav Skotarenko <vladskotarenko6227@gmail.com>
2024-07-18 22:35:34 +0300

    Initial Commit

---
+++ PassMan/PassManApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassManApp.swift
+//  PassMan
+//
+//  Created by Vladislav Skotarenko on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PassManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
