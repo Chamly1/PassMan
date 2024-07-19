aef923ca49ff11dec9253de03da80237957c3e19
Vladyslav Skotarenko <vladskotarenko6227@gmail.com>
2024-07-19 17:57:07 +0300

    add focus passing between field in credential add view

--- PassMan/AddCredentialView.swift
+++ PassMan/AddCredentialView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case resource
+    case username
+    case password
+}
+
 struct AddCredentialView: View {
     @EnvironmentObject var credentialsListViewModel: CredentialsListViewModel
     @Environment(\.dismiss) var dismiss
     @State private var inputResource: String = ""
     @State private var inputUsername: String = ""
     @State private var inputPassword: String = ""
+    @FocusState private var focusedField: Field?
     
     var body: some View {
         VStack(alignment: .trailing, spacing: 10) {
@@ -25,11 +32,26 @@ struct AddCredentialView: View {
             TextField("Resource name (site, application, etc.)", text: $inputResource)
                 .textFieldStyle(.roundedBorder)
                 .autocorrectionDisabled(true)
+                .focused($focusedField, equals: .resource)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .username
+                }
             TextField("Username/Login", text: $inputUsername)
                 .textFieldStyle(.roundedBorder)
                 .autocorrectionDisabled(true)
+                .focused($focusedField, equals: .username)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .password
+                }
             SecureField("Password", text: $inputPassword)
                 .textFieldStyle(.roundedBorder)
+                .focused($focusedField, equals: .password)
+                .submitLabel(.done)
+                .onSubmit {
+                    focusedField = nil
+                }
             Button("Add") {
                 credentialsListViewModel.addCredential(resource: inputResource, username: inputUsername, password: inputPassword)
                 dismiss()
