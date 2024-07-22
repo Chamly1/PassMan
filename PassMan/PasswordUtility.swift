5a5a3ad37addb9b43e0809eafd448703296bf4f2
Vladyslav Skotarenko <vladskotarenko6227@gmail.com>
2024-07-23 00:37:57 +0300

    move to separate method checking whether password contains characters from each pool

--- PassMan/PasswordUtility.swift
+++ PassMan/PasswordUtility.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+enum PasswordEntropyError: Error {
+    case unexpectedSymbolOccurred
+}
+
 struct PasswordUtility {
     private static let passwordLength: Int = 14
     private static let characterPools: [String] = [
@@ -22,13 +26,34 @@ struct PasswordUtility {
         "`~-_=+[{]}\\|;:'\",<.>/? "
     ]
     
+    private static func containsCharactersFromPools(_ password: String) throws -> [Bool] {
+        var isContainCharacterFromPool: [Bool] = Array(repeating: false, count: characterPools.count)
+
+        for character in password {
+            var isCharacterFromPools: Bool = false
+            
+            for i in 0..<characterPools.count {
+                if characterPools[i].contains(character) {
+                    isCharacterFromPools = true
+                    isContainCharacterFromPool[i] = true
+                    continue
+                }
+            }
+            
+            if !isCharacterFromPools {
+                throw PasswordEntropyError.unexpectedSymbolOccurred
+            }
+        }
+        
+        return isContainCharacterFromPool
+    }
+    
     static func generatePassword() -> String {
         var resultPassword: String
         var isContainCharacterFromPool: [Bool]
         var isPasswordAppropriate: Bool
         repeat {
             resultPassword = ""
-            isContainCharacterFromPool = Array(repeating: false, count: characterPools.count)
             
             for _ in 0..<passwordLength {
                 let poolNum = Int.random(in: 0..<characterPools.count)
@@ -36,14 +61,14 @@ struct PasswordUtility {
                 let symbolIndex = characterPools[poolNum].index(characterPools[poolNum].startIndex, offsetBy: symbolNum)
                 
                 resultPassword.append(characterPools[poolNum][symbolIndex])
-                
-                isContainCharacterFromPool[poolNum] = true
             }
             
+            isContainCharacterFromPool = try! containsCharactersFromPools(resultPassword)
             isPasswordAppropriate = true
             for isContain in isContainCharacterFromPool {
                 if !isContain {
                     isPasswordAppropriate = false
+                    break
                 }
             }
         } while !isPasswordAppropriate
