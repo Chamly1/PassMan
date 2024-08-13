7db40055e3f5d7f8abaeb1bf46c344f73f58efd1
Vladyslav Skotarenko <vladskotarenko6227@gmail.com>
2024-08-13 22:37:20 +0300

    add AuthenticationViewModel logic without master key managment

--- PassMan/ViewModels/AuthenticationViewModel.swift
+++ PassMan/ViewModels/AuthenticationViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CryptoKit
 
 class AuthenticationViewModel: ObservableObject {
     @Published var isAuthenticated: Bool = false
@@ -19,16 +20,70 @@ class AuthenticationViewModel: ObservableObject {
         }
     }
     
-    private let hasMasterKeyKey: String = "hasMasterKey"
+    private var salt: Data? {
+        get {
+            return UserDefaults.standard.data(forKey: saltKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: saltKey)
+        }
+    }
     
-    func initializeMasterKey(password: String) {
-        // TODO: add proper logic
+    private var sealedBoxVerificationString: Data? {
+        get {
+            return UserDefaults.standard.data(forKey: sealedBoxVerificationStringKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: sealedBoxVerificationStringKey)
+        }
+    }
+    
+    private let hasMasterKeyKey: String = "hasMasterKey"
+    private let saltKey: String = "salt"
+    private let sealedBoxVerificationStringKey: String = "sealedBoxVerificationString"
+    private let saltLength: Int = 16
+    private let keyLength: Int = 32
+    private let verificationString = "verification"
+    
+    func initializeMasterKey(password: String) throws {
+        // generate salt
+        var saltArray = Array<UInt8>(repeating: 0, count: saltLength)
+        let status = SecRandomCopyBytes(kSecRandomDefault, saltLength, &saltArray)
+        if status != errSecSuccess {
+            throw PassManError.saltGenerationError
+        }
+        
+        // derive key
+        // TODO: use PBKDF2?
+        let inputKeyMaterial = SymmetricKey(data: Data(password.utf8))
+        let derivedKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKeyMaterial, salt: saltArray, outputByteCount: keyLength)
+
+        // encrypt verification string
+        let verificationData = Data(verificationString.utf8)
+        let sealedBox = try ChaChaPoly.seal(verificationData, using: derivedKey)
+        
+        salt = Data(saltArray)
+        sealedBoxVerificationString = sealedBox.combined
+        
         isAuthenticated = true
         hasMasterKey = true
     }
     
-    func retrieveMasterKey(password: String) {
-        // TODO: add proper logic
+    func retrieveMasterKey(password: String) throws {
+        guard let saltData = salt else {
+            throw PassManError.noSalt
+        }
+        
+        let inputKeyMaterial = SymmetricKey(data: Data(password.utf8))
+        let derivedKey: SymmetricKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKeyMaterial, salt: saltData, outputByteCount: keyLength)
+        
+        guard let sealedBoxData = sealedBoxVerificationString else {
+            throw PassManError.noSealedBoxVerificationString
+        }
+        
+        // if authentication fail throws an error
+        _ = try ChaChaPoly.open(ChaChaPoly.SealedBox(combined: sealedBoxData), using: derivedKey)
+        
         isAuthenticated = true
     }
 }
