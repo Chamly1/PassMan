3a73b2f8ab5f0bde3823465beec10e2c7baba8af
Vladyslav Skotarenko <vladskotarenko6227@gmail.com>
2024-08-27 03:37:57 +0300

    add SettingsViewModel with functionality to control Face ID authentication

--- PassMan/ViewModels/SettingsViewModel.swift
+++ PassMan/ViewModels/SettingsViewModel.swift
@@ -5,14 +5,32 @@
 //  Created by Vladislav Skotarenko on 26.08.2024.
 //
 
-import SwiftUI
+import Foundation
 
-struct SettingsViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class SettingsViewModel: ObservableObject {
+    var isFaceIDEnabled: Bool {
+        get { UserDefaults.standard.bool(forKey: isFaceIDEnabledKey)}
     }
-}
-
-#Preview {
-    SettingsViewModel()
+    
+    private var credentialsViewModel: CredentialsViewModel
+    private var authenticationViewModel: AuthenticationViewModel
+    private var isFaceIDEnabledKey: String = "isFaceIDEnabled"
+    
+    init(credentialsViewModel: CredentialsViewModel, authenticationViewModel: AuthenticationViewModel) {
+        self.credentialsViewModel = credentialsViewModel
+        self.authenticationViewModel = authenticationViewModel
+    }
+    
+    func enableFaceID() throws {
+        let key = try credentialsViewModel.getEncryptionKey()
+        try authenticationViewModel.saveMasterKeyWithBiometry(key)
+        
+        UserDefaults.standard.setValue(true, forKey: isFaceIDEnabledKey)
+    }
+    
+    func disableFaceID() throws {
+        try authenticationViewModel.deleteMasterKeyWithBiometry()
+        
+        UserDefaults.standard.setValue(false, forKey: isFaceIDEnabledKey)
+    }
 }
